364e3ac1e29ed46963da1a44a7209a1b9c852883
AtharvNatu <natuatharv21@gmail.com>
2026-04-21 02:21:43 +0530

    Vulkan-Win32: Initial Commit For Sinewave Compute Shader

---
+++ 03-Vulkan/01-Features/02-SineWave/04-Compute/Build.bat
@@ -0,0 +1,105 @@
+
+@echo off
+
+set API=Vulkan
+
+set VULKAN_INCLUDE_PATH="C:\\VulkanSDK\\Vulkan\\Include"
+set VULKAN_LIB_PATH="C:\\VulkanSDK\Vulkan\\Lib"
+set VULKAN_BIN_PATH="C:\\VulkanSDK\\Vulkan\\Bin"
+
+set SOURCE_PATH=Source
+set INCLUDE_PATH=Include
+set IMAGES_PATH=Assets\Images
+
+set BIN_DIR=Bin
+set SPV=1
+
+if not exist %BIN_DIR% mkdir %BIN_DIR%
+
+cls
+
+if exist %BIN_DIR%\*.obj del /q %BIN_DIR%\*.obj >nul 2>&1
+if exist %BIN_DIR%\*.exe del /q %BIN_DIR%\*.res >nul 2>&1
+if exist %BIN_DIR%\*.res del /q %BIN_DIR%\*.exe >nul 2>&1
+
+echo ----------------------------------------------------------------------------------------------------------------
+echo Compiling %API% and Win32 Source Code ...
+echo ----------------------------------------------------------------------------------------------------------------
+        
+cl.exe  /c ^
+        /EHsc ^
+        /I %VULKAN_INCLUDE_PATH% ^
+        /I %VULKAN_INCLUDE_PATH%\glm ^
+        /I %INCLUDE_PATH% ^
+        %SOURCE_PATH%\Vk.cpp
+
+if errorlevel 1 (
+        @echo:
+        echo Compilation Failed !!!
+        exit /b 1
+)
+
+move *.obj %BIN_DIR% >nul 2>&1
+
+@echo:
+echo ----------------------------------------------------------------------------------------------------------------
+echo Compiling Resource Files ...
+echo ----------------------------------------------------------------------------------------------------------------
+rc.exe ^
+        /I %INCLUDE_PATH% ^
+        /I %IMAGES_PATH%^
+        /fo %BIN_DIR%\Vk.res ^
+        Assets\Vk.rc
+
+if errorlevel 1 (
+        @echo:
+        echo Resource Compilation Failed !!!
+        exit /b 1
+)
+
+@echo:
+if %SPV%==1 (
+    echo ----------------------------------------------------------------------------------------------------------------
+    echo Compiling Shader Files To SPIR-V Binaries ...
+    echo ----------------------------------------------------------------------------------------------------------------
+    cd Shaders
+    %VULKAN_BIN_PATH%\glslangValidator.exe -V -H -o Shader.vert.spv Shader.vert
+    %VULKAN_BIN_PATH%\glslangValidator.exe -V -H -o Shader.frag.spv Shader.frag
+    %VULKAN_BIN_PATH%\glslangValidator.exe -V -H -o Shader.comp.spv Shader.comp
+    move Shader.vert.spv ../%BIN_DIR%
+    move Shader.frag.spv ../%BIN_DIR%
+    move Shader.comp.spv ../%BIN_DIR%
+    cd ..
+    if errorlevel 1 (
+        @echo:
+        echo Shader Compilation Failed !!!
+        exit /b 1
+)
+)
+
+@echo:
+echo ----------------------------------------------------------------------------------------------------------------
+echo Linking Libraries and Resources...
+echo Creating Executable...
+echo ----------------------------------------------------------------------------------------------------------------
+link.exe ^
+        /OUT:%BIN_DIR%\Vk.exe ^
+        %BIN_DIR%\*.obj ^
+        %BIN_DIR%\Vk.res ^
+        /LIBPATH:%VULKAN_LIB_PATH% ^
+        user32.lib gdi32.lib^
+        /SUBSYSTEM:WINDOWS
+
+if errorlevel 1 (
+        @echo:
+        echo Linking Failed !!!
+        exit /b 1
+)
+
+@REM @echo:
+@REM echo ----------------------------------------------------------------------------------------------------------------
+@REM echo Launching Application ...
+@REM echo ----------------------------------------------------------------------------------------------------------------
+@REM cd %BIN_DIR%
+@REM Vk.exe
+@REM cd ..
